71f82e228a5860d190d3988c611c6cb3bbaa9cf8
Prompy <phawach4prompt@gmail.com>
2026-06-24 08:37:07 +0700

    Update main.py

--- main.py
+++ main.py
@@ -22,21 +22,5 @@ def downloads() -> str:
     return render_template('downloads.html', versions=versions)
 
 
-@app.route('/download/latest')
-def download_latest() -> Response:
-    file_path = os.path.join(VERSIONS_DIR, LATEST_DOWNLOAD)
-    if not os.path.isfile(file_path):
-        abort(404)
-    return send_from_directory(VERSIONS_DIR, LATEST_DOWNLOAD, as_attachment=True, mimetype='application/octet-stream')
-
-
-@app.route('/download/<path:filename>')
-def download_file(filename: str) -> Response:
-    file_path = os.path.join(VERSIONS_DIR, filename)
-    if not os.path.isfile(file_path):
-        abort(404)
-    return send_from_directory(VERSIONS_DIR, filename, as_attachment=True, mimetype='application/octet-stream')
-
-
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000, debug=True)
